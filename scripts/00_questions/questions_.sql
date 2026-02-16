0a239c23fdfe5910071d14ead81bfe3ed7babbf4
Subham Adhikari <165596073+Subham-Adhikari@users.noreply.github.com>
2026-02-17 02:51:13 +0530

    Create questions_.sql

---
+++ scripts/00_questions/questions_.sql
@@ -0,0 +1,19 @@
+/*
+    Database initialization & exploration,
+    Data understanding (schema, keys, grain),
+    Dimension exploration,
+    Date exploration,
+    Measure exploration,
+
+    Magnitude analysis,
+    Top-N / Bottom-N analysis,
+
+    Change-over-time analysis (MoM, YoY, DoD),
+    Cumulative analysis (Running Total: MTD, QTD, YTD),
+    Performance analysis (Moving / Rolling Totals for trend analysis),
+
+    Part-to-whole analysis,
+    Data segmentation (CASE WHEN ... THEN ... ELSE ... END),
+
+    Report preparation (Customer-level report, Product-level report)
+*/
